afb5639b51ad44aa53dcabe187ed9d7c95db49db
sudip3711 <149355694+sudip3711@users.noreply.github.com>
2025-05-20 16:35:40 +0530

    Delete Quiz directory

--- Quiz/src/main/java/com/sudip/Quiz/Model/QuizSummaryForGetAll.java
+++
@@ -1,9 +0,0 @@
-package com.sudip.Quiz.Model;
-
-import java.time.LocalDateTime;
-
-public interface QuizSummaryForGetAll {
-    Long getId();
-    String getTitle();
-    LocalDateTime getCreatedDate();
-}
